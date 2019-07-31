3cedcac29f3d2a9115fb9552f1fb6c2bfe5d11c5
kucuk13 <52529079+kucuk13@users.noreply.github.com>
2019-07-31 17:22:01 +0300

    Draws n tangent circles on the screen with the entered values(x, y, radius, n).

--- app/src/main/java/com/example/proje/MainActivity.java
+++ app/src/main/java/com/example/proje/MainActivity.java
@@ -8,22 +8,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
+    public static int a;
+    public static int b;
+    public static int c;
+    public static int d;
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void draw(View v){
+        final EditText xe = (EditText) findViewById(R.id.x);
+        final EditText ye = (EditText) findViewById(R.id.y);
+        final EditText re = (EditText) findViewById(R.id.r);
+        final EditText ne = (EditText) findViewById(R.id.n);
+        a = Integer.parseInt(xe.getText().toString());
+        b = Integer.parseInt(ye.getText().toString());
+        c = Integer.parseInt(re.getText().toString());
+        d = Integer.parseInt(ne.getText().toString());
         setContentView(new MyView(this));
     }
 
     public class MyView extends View
     {
+
         Paint paint = null;
         public MyView(Context context)
         {
@@ -35,17 +52,24 @@ public class MainActivity extends AppCompatActivity
         protected void onDraw(Canvas canvas)
         {
             super.onDraw(canvas);
-            int x = 300;
-            int y = 400;
+            int x = a;
+            int y = b;
             int radius;
-            radius = 50;
-            paint.setStyle(Paint.Style.STROKE); //FILL = doldur, STROKE = sadece kenarlarını çiz
+            radius = c;
+            paint.setStyle(Paint.Style.FILL);
             paint.setColor(Color.WHITE);
             canvas.drawPaint(paint);
-            // Use Color.parseColor to define HTML colors
             paint.setColor(Color.BLACK);
             canvas.drawCircle(x / 2, y / 2, radius, paint);
-            //canvas.drawCircle(200, 300, radius, paint);
+            int i ;
+            d = d - 1;
+            for (i = 0; i < d; i++){
+                x = x + (c * 4);
+                paint.setStyle(Paint.Style.STROKE);
+                canvas.drawCircle(x / 2, y / 2, radius, paint);
+            }
         }
     }
+
+
 }
